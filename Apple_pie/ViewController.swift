e1205270525fea8feda866ac3236e725f712038e
Programming <programming@macbook-pro-programming-5.local>
2019-02-02 16:15:40 +0300

    UI finished, IPad turns are working

--- Apple_pie/ViewController.swift
+++ Apple_pie/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Apple_pie
 //
-//  Created by Programming on 02.02.2019.
+//  Created by Andrew Mamontov on 02.02.2019.
 //  Copyright © 2019 MAPF games. All rights reserved.
 //
 
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if size.width < size.height {
+            topStackView.axis = .vertical
+        } else {
+            topStackView.axis = .horizontal
+        }
+    }
 
 }
 
